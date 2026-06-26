b6a5b8200afca1d4cdc564dfeffd026c91a9a4b2
2pt9bp9rgz-coder <mio4_28@icloud.com>
2026-06-26 19:31:58 +0900

    メンバーページ作成

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        taupe: {
+          50: '#f5f3f0',
+          100: '#ede8e3',
+          200: '#ddd4ca',
+          300: '#cbb9ad',
+          400: '#b8a098',
+          500: '#a68784',
+          600: '#8b6d62',
+          700: '#725849',
+          800: '#5a4839',
+          900: '#48372a',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
